d66293ceaadded8854ac90d229023ede85df8819
Zachary Waweru <azwdevops@gmail.com>
2024-03-25 22:11:27 +0300

    added javascript (to enable changin theme, and show/closing sidebar on small devices,  and added media queries integration

---
+++ README.md
@@ -0,0 +1,5 @@
+- a beautiful admin dashboard
+  ![alt text](dashboard.png)
+  ![alt text](dashboard1.png)
+  ![alt text](dashboard2.png)
+  ![alt text](dashboard3.png)
---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "84573",
+    paymentStatus: "Refunded",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lavender S45 Drone",
+    productNumber: "96575",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with camera",
+    productNumber: "25377",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS Tracker Drone",
+    productNumber: "47524",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Ruko 578 Mini Drone",
+    productNumber: "22427",
+    paymentStatus: "Refunded",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lowema Mini Drone",
+    productNumber: "77585",
+    paymentStatus: "Paid",
+    shipping: "Declined",
+  },
+  {
+    productName: "DJI Mini Drone",
+    productNumber: "42244",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "KWS Mini Drone",
+    productNumber: "85227",
+    paymentStatus: "Paid",
+    shipping: "Pending",
+  },
+  {
+    productName: "JHU Mini Drone",
+    productNumber: "25577",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "OLK Mini Drone",
+    productNumber: "85824",
+    paymentStatus: "Paid",
+    shipping: "Pending",
+  },
+];
